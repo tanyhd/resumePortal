e12442498a9e78e5967908c5dd68fdbd70f53a98
Darren <darren243@hotmail.com>
2023-02-10 12:14:02 +0800

    Initial commit

---
+++ src/test/java/myapp/resume/portal/ResumePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package myapp.resume.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
